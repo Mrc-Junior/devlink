2288f3687a7540bdb31d15557598d2cd006044fb
Marco Junior <50880274+Mrc-Junior@users.noreply.github.com>
2024-02-13 14:44:54 -0300

    :tada: initial comit

---
+++ assets/scripts/index.js
@@ -0,0 +1,23 @@
+function toggle() {
+  const html = document.documentElement
+  if(html.classList.contains('light')) {
+    html.classList.remove('light')
+  } else {
+    html.classList.add('light');
+  }
+  const img = document.querySelector('#profile img')
+  const desc = document.querySelector("#profile .text")
+  // Update img attribute
+  if(html.classList.contains('light')) {
+    img.setAttribute("src","/assets/images/avatar-light.jpg");
+  } else {
+    img.setAttribute("src","/assets/images/avatar.jpg");
+  }
+
+  // Update text
+  if(html.classList.contains('light')) {
+    desc.textContent = "@Mrc.Junior";
+  } else {
+    desc.textContent = "Contact : #mrcjuuh";
+  }
+}
